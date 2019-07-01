faf9eb211e1c0c486553974e01a40fc4f91a3d7e
Nestor Escoto <nescoto@loe.corp>
2019-07-01 12:08:28 -0600

    Updating test for Escrow

--- test/escrow.js
+++ test/escrow.js
@@ -7,6 +7,8 @@ contract("Escrow", accounts => {
   const bob = accounts[1];
 
   let instance;
+  
+  const AMOUNT = 2000;
 
   beforeEach(async() => {  
     instance = await Escrow.new();
@@ -27,12 +29,12 @@ contract("Escrow", accounts => {
   describe('Contract Functionality', () => {
     describe('Escrow Creation', () => {
       it('the buyer should be able to create a new escrow with a minimum of 100 weis', async () => {
-        const receipt = await instance.addEscrow(bob, 100, {from: alice});
+        const receipt = await instance.addEscrow(bob, {from: alice, amount: 100});
         assert.equal(receipt, 0, 'the escrow should be created');
       });
 
       it('the buyer should no be able to create a new escrow with a value less than 100 weis', async() => {
-        await catchRevert(instance.addEscrow(bob, 99, { from: alice }));
+        await catchRevert(instance.addEscrow(bob, { from: alice, amount: 99}));
       });
 
       it('should match the information in getEscrow()', async () => {
@@ -65,17 +67,17 @@ contract("Escrow", accounts => {
     describe('Escrow Payment', () => {
       describe('should not allow the seller to withdraw', () => {
         it('the escrow did not meet the conditions', async() => {
-          const receipt = await instance.add(bob, 2000);
+          const receipt = await instance.addEscrow(bob, { from: alice, value: AMOUNT });
           await instance.vote(receipt, false, {from: alice});
           await instance.vote(receipt, true, {from: bob});
           await catchRevert(instance.withdraw(receipt, {from: bob}));
         });
         it('the escrow is open', async() => {
-          const receipt = await instance.add(bob, 2000)
+          const receipt = await instance.addEscrow(bob, { from: alice, value: AMOUNT });
           await catchRevert(instance.withdraw(receipt, { from: bob }));
         });
         it('the escrow is already withdrawn', async() => {
-          const receipt = await instance.add(bob, 2000);
+          const receipt = await instance.addEscrow(bob, { from: alice, value: AMOUNT });
           await instance.vote(receipt, true, { from: alice });
           await instance.vote(receipt, true, { from: bob })
           await instance.withdraw(receipt, {from: bob});
@@ -85,17 +87,17 @@ contract("Escrow", accounts => {
 
       describe('should now allow the owner to transfer if', () => {
         it('the escrow did meet the conditions', async() => {
-          const receipt = await instance.add(bob, 2000);
+          const receipt = await instance.addEscrow(bob, { from: alice, value: AMOUNT });
           await instance.vote(receipt, true, { from: alice });
           await instance.vote(receipt, true, { from: bob });
           await catchRevert(instance.transferWhenBlocked(receipt, bob, {from: creator}));
         });
         it('the escrow is open', async() => {
-          const receipt = await instance.add(bob, 2000);
+          const receipt = await instance.addEscrow(bob, { from: alice, value: AMOUNT });
           await catchRevert(instance.transferWhenBlocked(receipt, alice, {from: creator}));
         });
         it('the address provided is not buyer nor seller', async() => {
-          const receipt = await instance.add(bob, 2000);
+          const receipt = await instance.addEscrow(bob, { from: alice, value: AMOUNT });
           await instance.vote(receipt, false, { from: alice });
           await instance.vote(receipt, true, { from: bob });
           await catchRevert(instance.transferWhenBlocked(receipt, accounts[5], { from: creator }));
@@ -104,7 +106,7 @@ contract("Escrow", accounts => {
 
       describe('should allow the seller to withdraw if', () => {
         it('the scrow met the conditions', async() => {
-          const receipt = await instance.add(bob, 2000);
+          const receipt = await instance.addEscrow(bob, { from: alice, value: AMOUNT });
           const bobBalance = await web3.eth.getBalance(bob);
           await instance.vote(receipt, true, { from: alice });
           await instance.vote(receipt, true, { from: bob });
@@ -116,7 +118,7 @@ contract("Escrow", accounts => {
 
       describe('should allow the creator to transfer if', () => {
         it('the scrow did not meet the conditions', async() => {
-          const receipt = await instance.add(bob, 2000);
+          const receipt = await instance.addEscrow(bob, { from: alice, value: AMOUNT });
           const aliceBalance = await web3.eth.getBalance(alice);
           await instance.vote(receipt, false, { from: alice });
           await instance.vote(receipt, true, { from: bob });
@@ -126,6 +128,81 @@ contract("Escrow", accounts => {
         });
       });
     });
+
+    describe('Events', () => {
+      it('should emit an event when a new escrow is created', async() => {
+        const amount = 100;
+        const receipt = await instance.addEscrow(bob, { from: alice, value: amount });
+        const expectedBuyer = receipt.logs[0].args.buyerAddress;
+        const expectedSeller = receipt.logs[0].args.sellerAddress;
+        const expectedAmount = receipt.logs[0].args.amount.toNumber();
+        assert.equal(alice, expectedBuyer, "LogEscrowCreated event buyerAddress not emitted correctly");
+        assert.equal(bob, expectedSeller, "LogEscrowCreated event sellerAddress not emitted correctly");
+        assert.equal(amount, expectedAmount, "LogEscrowCreated event amount not emitted correctly");
+      });
+
+      it('should emit an event when a vote happens', async() => {
+        const receipt = await instance.addEscrow(bob, { from: alice, value: AMOUNT });
+        const buyerVote = await instance.vote(receipt, true, { from: alice });
+        let expectedAddress = buyerVote.logs[0].args.address;
+        let expectedVote = buyerVote.logs[0].args.vote;
+        let expectedId = buyerVote.logs[0].args.id.toNumber();
+        assert.equal(alice, expectedAddress, "LogVote event address not emitted correctly");
+        assert.equal(expectedVote, true, "LogVote event vote not emitted correctly");
+        assert.equal(expectedId, receipt, "LogVote event id not emitted correctly");
+        const sellerVote = await instance.vote(receipt, false, { from: bob });
+        expectedAddress = sellerVote.logs[0].args.address;
+        expectedVote = sellerVote.logs[0].args.vote;
+        expectedId = sellerVote.logs[0].args.id.toNumber();
+        assert.equal(bob, expectedAddress, "LogVote event address not emitted correctly");
+        assert.equal(expectedVote, false, "LogVote event vote not emitted correctly");
+        assert.equal(expectedId, receipt, "LogVote event id not emitted correctly");        
+      });
+
+      it('should emit an event when a withdraw happens', async() => {
+        const receipt = await instance.addEscrow(bob, { from: alice, value: AMOUNT });
+        await instance.vote(receipt, true, { from: alice });
+        await instance.vote(receipt, true, { from: bob })
+        const results = await instance.withdraw(receipt, { from: bob });
+        const expectedAddress = results.logs[0].args.address;
+        const expectedAmount = results.logs[0].args.amount.toNumber();
+        const expectedId = results.logs[0].args.id.toNumber();
+        assert.equal(bob, expectedAddress, "LogWithdraw event address not emitted correctly");
+        assert.equal(expectedAmount, AMOUNT, "LogWithdraw event amount not emitted correctly");
+        assert.equal(expectedId, receipt, "LogWithdraw event id not emitted correctly");  
+      });
+
+      it('should emit an event when the creator transfer the amount', async() => {
+        const receipt = await instance.addEscrow(bob, { from: alice, value: AMOUNT });
+        await instance.vote(receipt, false, { from: alice });
+        await instance.vote(receipt, true, { from: bob });
+        const results = await instance.transferWhenBlocked(receipt, alice, { from: creator });
+        const expectedAddress = results.logs[0].args.address;
+        const expectedAmount = results.logs[0].args.amount.toNumber();
+        const expectedId = results.logs[0].args.id.toNumber();
+        assert.equal(alice, expectedAddress, "LogTransfer event address not emitted correctly");
+        assert.equal(expectedAmount, AMOUNT, "LogTransfer event amount not emitted correctly");
+        assert.equal(expectedId, receipt, "LogTransfer event id not emitted correctly");  
+      });
+    });
+
+    describe('Design Pattern', () => {
+      it('should have a public accesible variable to handle circuit breaker pattern initialized on false', async () => {
+        const stopped = await instance.stopped();
+        assert.equal(stopped, false, 'stopped should be false');
+      });
+
+      it('only creator should be able to change the stopped variable', async () => {
+        await instance.setStopped(true, {from: creator});
+        const stopped = await instance.stopped();
+        assert.equal(stopped, true, 'stopped should be true');
+        await catchRevert(instance.setStopped(true, {from: alice}));
+      });
+
+      it('should disallow Escrow creation when stopped', async () => {
+        await catchRevert(instance.addEscrow(bob, { from: alice, value: AMOUNT }));
+      });
+    }); 
     
   });
 });
